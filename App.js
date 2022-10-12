6d7246194061518bc7ca251e05a5e60925a25937
Joakim Andersson <joakim.andersson2@student.kyh.se>
2022-10-12 14:12:59 +0200

    Jos code

--- App.js
+++ App.js
@@ -1,10 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Audio } from "expo-av";
+import { View,
+  Text,
+  StyleSheet,
+  Button} from 'react-native';
+import React from 'react';
+import { musiclibrary } from './data';
+
 
 export default function App() {
+const sound = new Audio.Sound()
+let channelsArray = []
+
+const fetchList2 = async () => {
+  try {
+    let response = await fetch("http://api.sr.se/api/v2/channels?format=json%22");
+    let json = await response.json();
+    console.log(json.channels[0].liveaudio.url)
+    channelsArray = json.channels
+    console.log(json.channels)
+    console.log({channelsArray})
+    return json;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function loadSound(uri) {
+  await sound.loadAsync({
+    uri: uri
+  })
+}
+
+async function playSound() {
+  await sound.playAsync()
+}
+
+async function pause() {
+  await sound.pauseAsync()
+}
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title='Fetch list' onPress={fetchList2}></Button>
+      <Button title="Load audio" onPress={() => {loadSound(musiclibrary[0].url)}}></Button>
+      <Button title="Play audio" onPress={playSound}></Button>
+      <Button title="Pause audio" onPress={pause}></Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,5 +60,3 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-
-//git test
