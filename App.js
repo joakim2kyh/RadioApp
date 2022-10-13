d999cc86a94288221ae67fc45ea0aa0010c905ab
jojojokyh <joanne.yager@student.kyh.se>
2022-10-13 15:23:12 +0200

    navigation tabs - first step

--- App.js
+++ App.js
@@ -7,62 +7,15 @@ import { View,
   Image,
   FlatList} from 'react-native';
 import React, {useState} from 'react';
+import MainContainer from './navigation/MainContainer';
 
 
 export default function App() {
-const sound = new Audio.Sound()
-const [channels, setChannels] = useState([])
-let musiclibrary = []
 
-const fetchList2 = async () => {
-  try {
-    let response = await fetch("http://api.sr.se/api/v2/channels?format=json");
-    let json = await response.json();
-    setChannels(json.channels)
-    return json;
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-async function loadSound(uri) {
-  await sound.loadAsync({
-    uri: uri
-  })
-}
-
-async function playSound() {
-  await sound.playAsync()
-}
-
-async function pause() {
-  await sound.pauseAsync()
-}
 
   return (
-    <View style={styles.container}>
-      
-      <Text>Open up App.js to start working on your app!</Text>
-      <Button title='Fetch list' onPress={fetchList2}></Button>
-      <FlatList
-        data={channels}
-        renderItem={({ item }) => (
-          <Text style={styles.item}>{item.name}</Text>
-        )}
-      />
-    </View>
+    <MainContainer/>
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    padding: 20,
-  },
-  item: {
-    marginTop: 24,
-    padding: 30,
-    backgroundColor: "pink",
-  }
-});
---
+++ navigation/MainContainer.js
@@ -0,0 +1,68 @@
+import { StatusBar } from 'expo-status-bar';
+import { Audio } from "expo-av";
+import { View,
+  Text,
+  StyleSheet,
+  Button,
+  Image,
+  FlatList} from 'react-native';
+import React, {useState} from 'react';
+
+export default function MainContainer() {
+
+    const sound = new Audio.Sound()
+    const [channels, setChannels] = useState([])
+    let musiclibrary = []
+
+    const fetchList2 = async () => {
+        try {
+            let response = await fetch("http://api.sr.se/api/v2/channels?format=json");
+            let json = await response.json();
+            setChannels(json.channels)
+            return json;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async function loadSound(uri) {
+        await sound.loadAsync({
+            uri: uri
+        })
+    }
+
+    async function playSound() {
+        await sound.playAsync()
+    }
+
+    async function pause() {
+        await sound.pauseAsync()
+    }
+    return (
+        <View style={styles.container}>
+
+            <Text>Open up App.js to start working on your app!</Text>
+            <Button title='Fetch list' onPress={fetchList2}></Button>
+            <FlatList
+                data={channels}
+                renderItem={({ item }) => (
+                    <Text style={styles.item}>{item.name}</Text>
+                )}
+            />
+        </View>
+
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      padding: 20,
+    },
+    item: {
+      marginTop: 24,
+      padding: 30,
+      backgroundColor: "pink",
+    }
+  });
