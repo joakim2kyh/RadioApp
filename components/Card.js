f2e9ec2247f5f21d07072f07189d7e7e7e72fa6a
Denis Rakitin <piggest@mail.ru>
2022-10-17 09:00:30 +0200

    data storage update

--- components/Card.js
+++ components/Card.js
@@ -1,30 +1,64 @@
 import React from "react";
 import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
+import { AntDesign } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
 
-function Card(props) {
-
-  function onPress(){
-    console.log("CLICK")
-  }
+export default Card = (props) => {
 
   return (
-      <TouchableOpacity style ={styles.card} onPress={onPress}>
-          <Image style= {styles.cardImage} source={{uri: props.item.image}}/> 
-          <Text style={styles.cardText}>{props.item.name}</Text>
-      </TouchableOpacity>
-  
+    <View style={styles.container}>
+      <View style={[styles.cardContainer, {backgroundColor: '#'+props.item.color}]}>
+        {/* <TouchableOpacity style ={styles.card} onPress={onPress}> */}
+        <Image style={styles.cardImage} source={{ uri: props.item.image }} />
+        <Text style={styles.cardText}>{props.item.name}</Text>
+        <View style={styles.buttonContainer}>
+        <TouchableOpacity onPress={()=>props.playRadio()}>
+          <AntDesign style={styles.play} name="play" size={30} color="black" />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={()=>props.addFavorite()}>
+          <MaterialIcons name="favorite-outline" size={30} color="black" />
+        </TouchableOpacity>
+        </View>
+      </View>
+    </View>
   );
 }
 
-export {Card}
+// export { Card }
 
 const styles = StyleSheet.create({
 
- // item: {
+  // item: {
   //  marginTop: 24,
-   // padding: 30,
-   // backgroundColor: "pink",
- // },
+  // padding: 30,
+  // backgroundColor: "pink",
+  // },
+  cardContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flex: 1,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 12,
+    borderColor: '#fff',
+    borderWidth: 1,
+    backgroundColor: '#219ebc',
+    marginBottom: 20,
+    shadowColor: 'black',
+    shadowColor: 'black',
+    shadowOpacity: 1,
+    shadowRadius: 1,
+    elevation: 5,
+    shadowOffset: {
+      width: 3,
+      height: 3
+    }
+  },
+  container: {
+    flexDirection: 'row',
+    marginHorizontal: 10,
+  },
   card: {
     backgroundColor: '#50A0B7',
     marginBottom: 20,
@@ -35,20 +69,31 @@ const styles = StyleSheet.create({
     shadowRadius: 1,
     elevation: 9,
     shadowOffset: {
-    width:3,
-      height:3
+      width: 3,
+      height: 3
     }
-   
   },
-  cardImage:{
-    width: '100%',
-    height:200,
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cardImage: {
+    // width: '100%',
+    height: 100,
+    width: 100,
+    borderRadius: 12,
+    padding: 15,
+    margin: 5,
     resizeMode: 'cover'
 
   },
-  cardText:{
+  cardText: {
     padding: 10,
-    fontSize: 20
-
+    fontSize: 20,
+    alignItems: 'center',
+  },
+  play: {
+    marginEnd: 10
   }
 });
--- navigation/screens/HomeScreen.js
+++ navigation/screens/HomeScreen.js
@@ -1,7 +1,8 @@
 import { View, Text, Button, FlatList, StyleSheet } from 'react-native';
 import { Audio } from "expo-av";
 import React, {useState, useEffect} from 'react';
-import {Card} from '../../components/Card';
+import Card from '../../components/Card';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 export function HomeScreen({navigation, component}) {
@@ -9,19 +10,59 @@ export function HomeScreen({navigation, component}) {
     const sound = new Audio.Sound()
     const [channels, setChannels] = useState([])
     const [schedule, setSchedule] = useState([])
+    const [favourites, setFavourites] = useState([])
     const [live, setLive] = useState("")
     let musiclibrary = []
 
+
+    const storeData = async (value) => {
+      try {
+        const jsonValue = JSON.stringify(value)
+        console.info(jsonValue)
+        await AsyncStorage.setItem('idArray', jsonValue)
+      } catch (e) {
+        console.log(e)
+      }
+    }
+
+    const getData = async () => {
+      try {
+        const jsonValue = await AsyncStorage.getItem('idArray')
+        console.info("data = " + JSON.parse(jsonValue))
+        return jsonValue != null ? JSON.parse(jsonValue) : [];
+      } catch(e) {
+        console.log(e)
+      }
+    }
+
+    const playRadio = ()=> {
+      getData()
+    }
+
+    const addFavorite = (item)=> {
+      console.log(item)
+      if (favourites.includes(item) == false){
+        setFavourites([...favourites, item])
+        console.log("added " + item)
+      } else {
+        
+        setFavourites(favourites.filter(name => name != item))
+        console.info("deleted item "  + item + " from " + favourites)
+      }
+    }
+
     useEffect(() => {
       fetchList2()
-  }, [])
+      console.info("effect " + favourites)
+      storeData(favourites)
+  }, [favourites])
 
     const fetchList2 = async () => {
         try {
             let response = await fetch("http://api.sr.se/api/v2/channels?format=json");
             let json = await response.json();
             setChannels(json.channels)
-            //console.log(channels)
+            // console.log(channels)
             return json;
         } catch (error) {
             console.error(error);
@@ -81,7 +122,7 @@ export function HomeScreen({navigation, component}) {
         <FlatList
             data={channels}
             renderItem={({ item }) => (
-              <Card item={item}/>
+              <Card item={item} playRadio = {()=>playRadio()} addFavorite={()=>addFavorite(item.id)}/>
             )}
             /> 
     </View>
