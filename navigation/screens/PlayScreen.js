5ea7fcee26cb25bef1712ae410bd9f7af1a0d08b
Joakim Andersson <joakim.andersson2@student.kyh.se>
2022-10-17 11:58:41 +0200

    item to playScreen

--- navigation/screens/PlayScreen.js
+++ navigation/screens/PlayScreen.js
@@ -1,12 +1,26 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
 
 export function PlayScreen({navigation, route}) {
     
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text>
-                This is {route.params.name} </Text>
+            <Image style={styles.cardImage} source={{ uri: route.params.item.image }} />
+            <Text> This is {route.params.item.name} </Text>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    cardImage: {
+    // width: '100%',
+    height: 100,
+    width: 100,
+    borderRadius: 12,
+    padding: 15,
+    margin: 5,
+    resizeMode: 'cover'
+
+  },
+
+});
