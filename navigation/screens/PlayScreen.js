2285e3ee3d0a89c4b863ec26496a99cb0a5bcae7
jojojokyh <joanne.yager@student.kyh.se>
2022-10-18 15:07:47 +0200

    program info in playscreen but we have a warning

--- navigation/screens/PlayScreen.js
+++ navigation/screens/PlayScreen.js
@@ -1,13 +1,19 @@
 
 import { View, Text, StyleSheet, Image, Button,TouchableOpacity } from 'react-native';
 import { Audio } from "expo-av";
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 
 export function PlayScreen({navigation, route}) {
 
     const sound = route.params.sound
     const [isPlaying, setPlaying] = useState(false)
+    const [schedule, setSchedule] = useState([])
+    const [live, setLive] = useState("")
+
+    useEffect(() => {
+        fetchSchedule(route.params.item.id)
+    })
 
     async function loadSound(uri) {
 
@@ -38,6 +44,38 @@ export function PlayScreen({navigation, route}) {
        
     }
 
+    const fetchSchedule = async (id) => {
+        const uri = `http://api.sr.se/v2/scheduledepisodes?channelid=${id}&format=json&pagination=false`
+
+        try {
+            const response = await fetch(uri);
+            let json = await response.json();
+           // let data = await Promise.all(json.)
+            setSchedule(json.schedule)
+            const now = Date.now()
+
+            // schedule.forEach(element => {
+
+            // for await (const element of schedule) {
+            //     let startTime = element.starttimeutc
+            //     startTime = startTime.slice(6, -2)
+            //     let endTime = element.endtimeutc
+            //     endTime = endTime.slice(6, -2)
+
+            //     if (startTime < now && endTime > now) {
+            //         // console.log(element.title)
+            //         // console.log(live)
+            //         setLive(element)
+            //     } else {
+            //         //console.log("NOPE") 
+            //     }
+
+            // };
+        } catch (error) {
+            console.error(error);
+        }
+      }
+
 
     async function playSound() {
        
@@ -52,10 +90,16 @@ export function PlayScreen({navigation, route}) {
     return (
 
         <View style={styles.container}>
-           <Image style={styles.channelImage} source={{ uri: route.params.item.image }}/>
+           <Image style={styles.channelImage} source={{ uri: live.imageurl }}/>
           
-            <Text style={styles.tagline}>
-                This is {route.params.item.tagline} </Text>
+            {/* <Text style={styles.tagline}>
+                This is {route.params.item.tagline} </Text> */}
+            <Text>
+                { route.params.item.name }
+            </Text>
+            <Text>
+                {live.name}
+            </Text>
             <TouchableOpacity>
            
             </TouchableOpacity>
@@ -77,7 +121,7 @@ const styles = StyleSheet.create({
         width: 250,
         borderRadius: 12,
         padding: 15,
-        marginBottom: 100,
+        marginBottom: 20,
         resizeMode: 'cover'
         
     },
