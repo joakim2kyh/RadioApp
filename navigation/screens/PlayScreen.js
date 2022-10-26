e600e5d4eea1047589c6890bdd17493089a2f02c
jojojokyh <joanne.yager@student.kyh.se>
2022-10-26 13:06:52 +0200

    progress bar implemented with static value

--- navigation/screens/PlayScreen.js
+++ navigation/screens/PlayScreen.js
@@ -8,6 +8,7 @@ import { PressableScale } from 'react-native-pressable-scale';
 import { shadow } from 'react-native-paper';
 import CommonDataManager from '../../components/CommonDataManager';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import ProgressBar from 'react-native-progress/Bar';
 
 
 export function PlayScreen({ navigation, route }) {
@@ -140,6 +141,7 @@ export function PlayScreen({ navigation, route }) {
       <Text style={styles.programDescripton}>
         {live.title}
       </Text>
+      <ProgressBar progress={0.5}/>
 
       <PressableScale onPress={() => {soundManager.playRadio(route.params.item, live), setRefresh({refresh: !refresh})}}>
 
